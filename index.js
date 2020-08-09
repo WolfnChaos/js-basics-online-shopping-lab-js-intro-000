f6e32f6ca76dce8b8d86e53041e66b7e53bb9d2c
Bradly Horn <wolfnchaos@gmail.com>
2020-08-09 23:32:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function setCart(c) {
 }
 
 function addToCart(item) {
-  price = Math.floor(Math.random() * Math.floor(100));
+  var price = Math.floor(Math.random() * Math.floor(100));
   var newItem = {itemName: item, itemPrice: price};
   cart.push(newItem);
 }
